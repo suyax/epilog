d83f725dcb878d7bfc3e4fb50bb6bb45b5b92c25
Julien Panopio <jcpanopio1@gmail.com>
2016-01-25 15:35:42 -0800

    Work on the backend routes and controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,1 +1,8 @@
 // Contains the controllers exported from all tables
+module.exports = {
+  // comments: require('../comments/commentController.js'),
+  moments: require('../moments/momentController.js'),
+  stories: require('../stories/storyController.js')
+  // tags: require('../tags/tagController.js'),
+  // users: require('../users/userController.js')
+};
---
+++ server/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var db = require('./models/config');
+var request = require('request');
+
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+   
+require('./config/routes.js')(app);
+
+app.use(express.static(path.join(__dirname, "/client")));
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+ console.log('Server listening on port ' + port);
+});  
+
+module.exports = app;
