e252126bbaf217bc1d5c0b8c7a698ad87268b0eb
Akash Trivedi <akashtrivedi@Akashs-MacBook-Air.local>
2016-01-27 15:45:19 -0800

    set up files for seeding dummydata

---
+++ server/config/dummydata.js
@@ -0,0 +1,9 @@
+var db = require('../db/dbModel');
+var http = require('http');
+var dummyStoryData = [
+  {"title": "my life", "description": "is amazing. i play dota all the time"},
+  {"title": "me and my gf", "description": "my mom introduced us. sure, whatever"},
+  {"title": "my family", "description": "the wu-tang clan"},
+  {"title": "me and marley", "description": "my pet otter"},
+  {"title": "me and suya", "description": "bruises everywhere"}
+];
--- server/server.js
+++ server/server.js
@@ -1,25 +1,32 @@
+//define requirements
 var express = require('express');
-var path = require('path');
 var bodyParser = require('body-parser');
+var path = require('path');
+var seedDummyData = require('./config/dummyData.js');
 var db = require('./db/dbModel');
-var request = require('request');
 
-
+//create instance of express server
 var app = express();
 
+//set up middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-   
-require('./config/routes.js')(app);
-
+//serve up static files upon initialization of server
 app.use(express.static(path.join(__dirname, "/client")));
 
+//listen on routes   
+require('./config/routes.js')(app);
+
+//set up port for server
 var port = process.env.PORT || 3000;
 
-db.init();
-
+//set server to list on port specified above
 app.listen(port, function(){
  console.log('Server listening on port ' + port);
 });  
 
+//create database
+db.init();
+
+//export server
 module.exports = app;
