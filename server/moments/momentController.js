fca00dcefffbf796a2d755f49a63e462914ec619
Akash Trivedi <akashtrivedi@Akashs-MacBook-Air.local>
2016-01-27 15:10:14 -0800

    built out temp storage/fs.writeStream functionality

--- server/moments/momentController.js
+++ server/moments/momentController.js
@@ -8,43 +8,49 @@ module.exports =  {
 
   addMoment: function (req, res){
     //Define Variables
-    //filePath --> temp location in file tree where we will dump images
-    var filePath = path.join(__dirname, tempStorage + "/" + Math.random()*10000 + ".jpeg");
     //momentData --> location within request object, where header information lives
-    console.log(req.headers);
     var momentData = JSON.parse(req.headers.momentdata);
-    //moment --> contains all of the pertinent information related to a moment inside of the header
-    var moment = {
-      url: filePath,
-      caption: momentData.caption,
-      storyid: momentData.storyid
-    }
-    //open a writestream on the filepath defined above
-    var writeStream = fs.createWriteStream(
-      filePath,
-      {flags: 'ax', encoding: 'base64'}
-    );
+    //uniqueMomentIdentifier --> creates unique identifier for each moment based on data provided (NOTE: this is temporary);
+    var uniqueMomentIdentifier = momentData.caption.split(" ").join("")+momentData.storyid;
+    //filePath --> temp location in file tree where we will dump images
+    var filePath = path.join(__dirname, tempStorage + "/" + uniqueMomentIdentifier + ".jpeg");
 
-    //add each of the incoming data chunks from the client to the filepath specified above
-    req.on('data', function(chunk){
-      writeStream.write(chunk);
-    });
-    //once the data has been received, call the add method in the momentModel to input the moment 
-    //data into the database
-    req.on('end', function(){
-      momentModel.add(moment)
-        .then(function (results){
-          res.status(201).json(results);
-        })
-        .catch(function (error){
-          res.status(404).json();
+    //check to see if file path exists...
+    fs.stat(filePath, function (err, file){
+      //if filepath already exists, end response 
+      if(file && file.isFile()){
+        res.end('image already exists');
+      }
+      //otherwise...
+      else {
+        //define moment object that contains info related to moment
+        var moment = {
+          url: filePath,
+          caption: momentData.caption,
+          storyid: momentData.storyid
+        }
+        //open up fileStream on filePath 
+        var writeStream = fs.createWriteStream(
+          filePath,
+          {flags: 'ax', encoding: 'base64'}
+        );
+
+        //add each of the incoming data chunks from the client to the filepath specified above
+        req.on('data', function (chunk){
+            writeStream.write(chunk);
+          });
+        //once the data has been received, call the add method in the momentModel to input the moment data into the database
+        req.on('end', function (){
+          momentModel.add(moment)
+            .then(function (results){
+              res.status(201).json(results);
+            })
+            .catch(function (error){
+              res.status(404).json();
+            });
         });
-    })
-
-    
-    //add path to momentInfo before using the moment model to add the data
-    //in momentInfo to the db
-
+      }
+    });
   },
 
   getAllMoments: function (req, res) {
