efddec3b4c021c089ed0231880505fc1918bccdd
Alex Wu <alexdwu86@gmail.com>
2016-01-28 15:16:58 -0800

    [refactor] Implement thunkFetch, make action creators return FSA format

--- client/app/containers/app.js
+++ client/app/containers/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react-native';
-import { createStore } from 'redux';
+import { createStore, applyMiddleWare } from 'redux';
 import { Provider } from  'react-redux';
+import thunk from 'redux-thunk';
 import EpiLogApp from './epilogApp';
 
 import reducers from '../reducers/index';
-const store = createStore(reducers);
+const createStoreWithMiddleware = applyMiddleware(thunk);
+const store = createStoreWithMiddleware(reducers);
 
 export default class App extends Component {
   render() {
