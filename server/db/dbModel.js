1d107b5010cfc00cef7a2926b7486a7007535da5
Akash Trivedi <akashtrivedi@Akashs-MacBook-Air.local>
2016-01-29 11:48:46 -0800

    set up rest of tables and relationships

--- server/db/dbModel.js
+++ server/db/dbModel.js
@@ -1,6 +1,37 @@
+//requirements
 var Sequelize = require('Sequelize');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/epilog';
 var sequelize = new Sequelize(connectionString);
+
+
+//individual tables in database
+var User = sequelize.define('users', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  first_name: {
+    type: Sequelize.STRING,
+    notNull: true
+  },
+  last_name: {
+    type: Sequelize.STRING,
+    notNull: true
+  },
+  email: {
+    type: Sequelize.STRING,
+    notNull: true
+  },
+  token: {
+    type: Sequelize.STRING,
+    notNull: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    notNull: true
+  }
+}, {timestamps: false });
 
 
 var Story = sequelize.define('stories', {
@@ -9,19 +40,14 @@ var Story = sequelize.define('stories', {
     primaryKey: true,
     autoIncrement: true
   },
-  
   title: {
     type: Sequelize.STRING,
     notNull: true
   },
-
   description: {
     type: Sequelize.STRING,
     notNull: true
   }
-
-  // Add characters later
-
 }, { timestamps: false });
 
 
@@ -31,31 +57,96 @@ var Moment = sequelize.define('moments', {
     primaryKey: true,
     autoIncrement: true    
   },
-
   url: {
     type: Sequelize.STRING,
     notNull: true
   },
-
   caption: {
     type: Sequelize.STRING,
     notNull: true
   }
 }, { timestamps: false });
 
-// var Comment
-// var Tag
-// var User
 
+var Tag = sequelize.define('tags', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    notNull: true
+  }
+}, {timestamps: false })
+
+
+var Comment = sequelize.define('comments', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  text: {
+    type: Sequelize.STRING,
+    notNull: true
+  }
+}, {timestamps: false })
+
+//join tables
+var Moments_Stories = sequelize.define('moments_stories', {
+
+});
+
+
+var Users_Stories = sequelize.define('users_stories', {
+
+});
+
+var Tags_Moments = sequelize.define('tags_moments', {
+
+
+});
+
+//initialize db function 
 var init = function () {
-  Moment.belongsTo(Story, { foreignKey: 'storyid' });
-  Story.hasMany(Moment, { foreignKey: 'storyid' });
+  //define relationship between moments and stories (M <-> M)
+  Moment.belongsToMany(Story, { through: Moments_Stories});
+  Story.belongsToMany(Moment, { through: Moments_Stories});
+
+  //define relationship between users and moments (1 -> M)
+  Moment.belongsTo(User, {foreignKey: 'userid'});
+  User.hasMany(Moment, {foreignKey: 'userid'});
+
+  //define relationship between stories and users (M <-> M)
+  User.belongsToMany(Story, { through: Users_Stories});
+  Story.belongsToMany(User, { through: Users_Stories});
+
+  //define relationship between moments and comments (1 -> M)
+  Comment.belongsTo(Moment, {foreignKey: 'momentid'});
+  Moment.hasMany(Comment, {foreignKey: 'momentid'});
+
+  //define relationship between users and comments (1 -> M)
+  Comment.belongsTo(User, {foreignKey: 'userid'});
+  User.hasMany(Comment, {foreignKey: 'userid'});
+
+  //define relationship between tags and moments (M <-> M)
+  Tag.belongsToMany(Moment, { through: Tags_Moments});
+  Moment.belongsToMany(Tag, { through: Tags_Moments});
+
+  //build out all tables
   sequelize.sync();
 };
 
 
 module.exports = {
+  User: User,
   Story: Story,
   Moment: Moment,
+  Tag: Tag,
+  Comment: Comment,
+  Moments_Stories: Moments_Stories,
+  Users_Stories: Users_Stories,
+  Tags_Moments: Tags_Moments,
   init: init,
 };
