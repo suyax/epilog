15f98233437ed8ed810ff5a653412d119ff7c556
Alex Wu <alexdwu86@gmail.com>
2016-02-01 20:58:58 -0800

    [refactor] implent thunk for story fetch

--- client/app/containers/epilogApp.js
+++ client/app/containers/epilogApp.js
@@ -21,34 +21,14 @@ import LogOut from '../components/logOut';
 
 //router for the app
 class EpiLogApp extends Component {
-
-  componentDidMount() {
-    this.fetchStories();
-  }
-
-  // this could be a thunk
-  fetchStories() {
-    const { storiesActions } = this.props;
-
-    storiesActions.fetchStories();
-    fetch('http://127.0.0.1:3000/api/stories')
-      .then((response) => response.json())
-      .then((responseData) => {
-        console.log('The Response Data: ', responseData.stories);
-        storiesActions.recieveStories(responseData.stories);
-      })
-      .catch((error)=> {
-        storiesActions.failureStories(error)
-      })
-      .done();
-  }
-
+  
   render() {
     // Be explicit about what is available as props
     const {
       viewControlState,
       viewControlActions,
       storiesState,
+      storiesActions,
       authState,
       authActions,
 
@@ -57,90 +37,56 @@ class EpiLogApp extends Component {
     switch (viewControlState.currentView) {
       case "HOME":
         return (
-          <Home
-          onLogOut={()=>{viewControlActions.setView('LOGOUT')}}
-          />)
+          <Home 
+          onLogOut={ () => {viewControlActions.setView('LOGOUT') }} 
+          />);
       case "LOGIN":
         return (
           <LogIn
-          successLoggedIn={
-            ()=>{
-            viewControlActions.setView('HOME')
-          }}
-          onSignUp={
-            ()=>{
-            console.log('onSungup')
-            viewControlActions.setView('SIGNUP')}}
-          />)
+          successLoggedIn={ () => { viewControlActions.setView('HOME') }}
+          onSignUp={ () => { viewControlActions.setView('SIGNUP') }} 
+          />);
       case "SIGNUP":
         return (
           <SignUp
-          successSignedUp={
-            ()=>{
-            viewControlActions.setView('HOME')
-          }}
-          onLogIn={()=>{viewControlActions.setView('LOGIN')}}
-          />)
+          successSignedUp={ () => { viewControlActions.setView('HOME') }}
+          onLogIn={ () => { viewControlActions.setView('LOGIN') }} 
+          />);
       case "LOGOUT":
-        return <LogOut
-          successLoggedOut={
-            ()=>{
-            viewControlActions.setView('LOGIN')
-          }}
-        />
+        return (
+          <LogOut
+          successLoggedOut={ () => { viewControlActions.setView('LOGIN') }} 
+          />);
       case "LIBRARY":
         return (
           <Library
           stories={storiesState}
-          onTouchImage={
-            asset =>{
-              viewControlActions.setView('STORY', {
-                asset: asset
-              })
-            }}
-            />
-          );
-
+          onLoad={storiesActions.fetchStories}
+          onTouchImage={ (asset) =>{ viewControlActions.setView('STORY', { asset: asset }) }} 
+          />);
       case "STORY":
         return (
           <Story
           asset={viewControlState.passedProps.asset}
-          onBack={()=>{viewControlActions.setView('LIBRARY')}}
+          onBack={ () => { viewControlActions.setView('LIBRARY') }} 
           />);
-
       case "NEW_STORY":
         return (
           <NewStory
           asset={viewControlState.passedProps.asset}
           onBack={()=>{viewControlActions.setView('EDIT_MOMENT')}}
-          />
-        );
-
+          />);
       case "CAPTURE":
         return (
           <Capture
-          onTouchImage={
-            (asset)=>{
-              viewControlActions.setView('EDIT_MOMENT', {
-                asset: asset
-              });
-            }}
-          />
-        );
-
+          onTouchImage={ (asset) => { viewControlActions.setView('EDIT_MOMENT', { asset: asset }); }} 
+          />);
       case "EDIT_MOMENT":
         return(<EditMoment
           asset={viewControlState.passedProps.asset}
           onCancel={()=>{viewControlActions.setView('CAPTURE')}}
-          onTouchImage={
-            (asset)=>{
-              viewControlActions.setView('NEW_STORY', {
-                asset: asset
-              });
-            }
-          }
+          onTouchImage={ (asset) => { viewControlActions.setView('NEW_STORY', { asset: asset }); }}
           />);
-
       default:
         return <LogIn />;
     }
@@ -151,11 +97,13 @@ export default connect(state => ({
     viewControlState: state.viewControl,
     storiesState: state.stories,
     authState: state.authControl,
+    Urls:state.Urls, // where the various url's are
   }),
   (dispatch) => ({
     viewControlActions: bindActionCreators(actions.viewControlActions, dispatch),
     storiesActions: bindActionCreators(actions.storiesActions, dispatch),
-    authActions: bindActionCreators(actions.authActions, dispatch)
+    authActions: bindActionCreators(actions.authActions, dispatch),
+    thunkFetch: bindActionCreators(actions.thunkFetch, dispatch),
   })
 )(EpiLogApp);
 
