35c153c81c66952b2f004faa2ac4be054d9fb8ed
Akash Trivedi <akashtrivedi@Akashs-MacBook-Air.local>
2016-02-06 09:14:03 -0800

    added comments

--- client/app/components/story.js
+++ client/app/components/story.js
@@ -17,28 +17,43 @@ import AutoCompleteHelper from './autoComplete.js';
 
 var Story = React.createClass({
 
+  //upon initialization...
   getInitialState: function() {
+    //grab the story object associated with the story rendered on the story view (note: this
+    //is passed in from the library view)
     const story = this.props.asset; 
+    
+    //define variables that will ultimately give us access to the story's moments and tags
     var moments = story.moments;
     var tagObjsByMoment = moments.map(function(momentObj){return momentObj['tags'];});
     var arrayOfTagObjectsForStory = tagObjsByMoment.reduce(function(aggregator,arrOfTags){return aggregator.concat(arrOfTags);}, []);
     var arrayOfTagNames = arrayOfTagObjectsForStory.map(function(tagObj){return tagObj['name'];});
-    
+    ; set these variables 
+
+    //set the variables defined above to the view's state
     return {
       //holds all of the story titles associated with a particular user
       story: story,
+      //all of the moments associated with the story
       moments: moments,
       tagObjsByMoment: tagObjsByMoment,
+      //all of the tags associated with the story
       arrayOfTagNames: arrayOfTagNames,
+      //moments filtered by tag; should START by being equal to ALL of the moments.
       filteredMoments: moments
     };
   },
 
+  //helper func that filters a story's moments based on tag name
   filterMoments: function(event){
+    //grab tag name entered into auto complete search field
     var tagToFilterBy = event.nativeEvent.text;
+    //if nothing has been entered, set filtered array to ALL moments
     if(tagToFilterBy === ""){
       this.setState({filteredMoments: this.state.moments});
       return; 
+    //otherwise, filter moments by tag name. if tag name doesn't match anything in db, return an empty array
+    //(i.e. nothing should be displayed on the page)
     } else {
       var copyOfMoments = this.state.moments.slice(0);
       var filtered = copyOfMoments.filter(function(moment){
