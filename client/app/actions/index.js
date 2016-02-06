ae57ca4641f6586b0408d34b36fdf8db92e589f8
Alex Wu <alexdwu86@gmail.com>
2016-02-05 17:21:25 -0800

    add momentViewControlActions to the actions index

--- client/app/actions/index.js
+++ client/app/actions/index.js
@@ -3,5 +3,6 @@ import * as viewControlActions from './viewControlActions';
 import * as thunkFetch from './thunkFetch';
 import * as storiesActions from './storiesActions';
 import * as authActions from './authActions.js';
+import * as momentViewControlActions from './momentViewControlActions';
 
-export {viewControlActions, thunkFetch, storiesActions, authActions};
+export {viewControlActions, thunkFetch, storiesActions, authActions, momentViewControlActions};
